2d1bd314b9aa738186420718b4f91841995c84cd
Ella Tanttu <ellajtanttu@gmail.com>
2021-10-25 15:56:40 -0700

    separate versions of js for different goals
    Liz Thomas <thomas.elizabeth.k@gmail.com>
    Jeff Terrell <jeffaterrell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//VERSION THAT MAKES ARRAY WITH A LOOP:
+
 $(document).ready(function() {
   $("#formFlavors").submit(function(event) {
     event.preventDefault();
@@ -6,8 +8,6 @@ $(document).ready(function() {
     numberArray.forEach(function(number) {
       inputArray.push($("'#flav" + number + "'").val());
     });
-
-    // let inputArray = [$("#flav1").val(),$("#flav2").val(),$("#flav3").val(),$("#flav4").val(),$("#flav5").val()];
 
     console.log("inputArray = " + inputArray);
 
@@ -18,16 +18,21 @@ $(document).ready(function() {
   });
 });
 
-/*
-> languages.forEach(function(element) {
-    alert('I love ' + element + '!');
+/* VERSION THAT AUTOMATICALLY MAKES ARRAY WITH INPUTS:
+
+$(document).ready(function() {
+  $("#formFlavors").submit(function(event) {
+    event.preventDefault();
+
+    let inputArray = [$("#flav1").val(),$("#flav2").val(),$("#flav3").val(),$("#flav4").val(),$("#flav5").val()];
+
+    console.log("inputArray = " + inputArray);
+
+    inputArray.forEach(function(element) {
+    // inputArray.push($("input").val());
+      $("ul").append("<li>" + element + "</li>");
+    });
   });
-
-
-
-const arrayOfThingsILike = [1, 2, 3, 4];
-arrayOfThingsILike.forEach(function(thing) {
-  $("#likable-things").append(" " + thing + "!");
 });
 
 */
